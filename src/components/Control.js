9c55b06479f042ee26e9b7f1e2c3128e1b708de4
soeun lee <thdmsdl87@gmail.com>
2020-04-24 22:43:09 +0900

    Implement Control, ReadContents, Toc

---
+++ src/components/Control.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+
+
+function Control(){
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Control;
---
+++ src/components/ReadContents.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+
+
+function ReadContents(){
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ReadContents;
