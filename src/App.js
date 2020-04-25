8125d48c209770ef2126e31e5ecd98746c5a7137
sung-oh-park <sung-oh.park@navercorp.com>
2020-04-25 13:55:17 +0900

    [#2] apply mode state

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import Toc from './components/Toc';
 import Control from './components/Control';
 import ReadContents from './components/ReadContents';
 
+// constants
+const MODE_WELCOME = 'welcome';
+const MODE_READ = 'read';
+const MODE_CREATE = 'create';
+const MODE_UPDATE = 'update';
+const MODE_DELETE = 'delete';
+
 function App() {
+  const [mode, setMode] = useState(MODE_WELCOME);
+
   return (
     <div className="App">
       <Subject></Subject>
       <Toc></Toc>
       <Control></Control>
-      <ReadContents></ReadContents>
+      {(mode===MODE_WELCOME || mode===MODE_READ) && <ReadContents></ReadContents>}
+      {mode===MODE_CREATE && <></>}
+      {mode===MODE_UPDATE && <></>}
+      {mode===MODE_DELETE && <></>}
     </div>
   );
 }
